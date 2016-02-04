46016303839f319d2733343c63bdcc2cf92091c7
Isak <aslundisak@gmail.com>
2016-02-04 21:59:06 +0100

    Updates

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ You can find a live version of this project [Here](http://elixir7.github.io/reac
 To try it out clone this repo. Run `npm install` and then run `http-server -p 3000`, then locate to localhost:3000/#/weather in your browser.
 
 ### Improvements
-Start-view for the user to read how it works and if they want to use Geolocation or search by city.
-Change background color depending on temperature.
-Add whole day forecast when clicking on that day.
+Start-view for the user to read how it works and if they want to use Geolocation or search by city. (check)
+Change background color depending on temperature or use the codedrop.com amazing background with realistic raindrops.
+Add whole day forecast when clicking on that day. (almost done)
 Add a spinner to when the app is finding the location and sending API request.
+Add more exact icons by checking id of icons [Here](http://openweathermap.org/weather-conditions)
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,22 +1,143 @@
 var React = require('react');
-var ReactRouter = require('react-router');
-var Link = ReactRouter.Link;
 
-var boxStyle = {
-  backgroundColor: "#1ab7ea"
+var popupDay = {
+  backgroundColor: "#1ab7ea",
+  height: "100%",
+  position: "absolute",
+  display: "none"
+};
+
+var timesIcon = {
+  marginTop: 20,
+  fontSize: 24
+}
+
+var infoIcon = timesIcon;
+
+var descIcon = {
+  fontSize: 24,
+  marginBottom: 20
+}
+
+var descIcon = {
+  fontSize: 24,
+  marginBottom: 20
+}
+
+var dayTitle = {
+  marginBottom: 50
+}
+
+var evalIcon = function(iconText){
+  var icon = "wi ";
+  if(iconText == "01d"){
+    icon += "wi-day-sunny";
+  } else if(iconText == "02d"){
+    icon += "wi-day-cloudy";
+  } else if(iconText == "03d"){
+    icon += "wi-cloud";
+  } else if(iconText == "04d"){
+    icon += "wi-cloudy";
+  } else if(iconText == "09d"){
+    icon += "wi-rain";
+  } else if(iconText == "10d"){
+    icon += "wi-day-rain";
+  } else if(iconText == "11d"){
+    icon += "wi-thunderstorm";
+  } else if(iconText == "13d"){
+    icon += "wi-snow";
+  } else if(iconText == "50d"){
+    icon += "wi-windy";
+  } else if(iconText == "01n"){
+    icon += "wi-night-clear";
+  } else if(iconText == "02n"){
+    icon += "wi-night-alt-cloudy";
+  } else if(iconText == "03n"){
+    icon += "wi-cloud";
+  } else if(iconText == "04n"){
+    icon += "wi-cloudy";
+  } else if(iconText == "09n"){
+    icon += "wi-rain";
+  } else if(iconText == "10n"){
+    icon += "wi-night-alt-rain";
+  } else if(iconText == "11n"){
+    icon += "wi-thunderstorm";
+  } else if(iconText == "13n"){
+    icon += "wi-snow";
+  } else if(iconText == "50n"){
+    icon += "wi-windy";
+  }
+  return (icon);
 };
 
 var Day = React.createClass({
-  render: function(){
-    return (
-      <div className="row">
-        <div className="col-md-4 text-center" style={boxStyle}>
-          <div className="row">
-            <div className="col-xs-10 col-xs-offset-1">
-              <h1>Day Test</h1>
-            </div>
-          </div>
 
+  render: function(){
+
+    var times = this.props.days.map(function(item, key){
+      if(item.dt_txt.substring(8, 10) == this.props.date.substring(4, 6)){
+        return(
+          <p key={key}>{item.dt_txt.substring(11,16)}</p>
+        );
+      }
+    }.bind(this));
+
+    var weatherIcons = this.props.days.map(function(item, key){
+      if(item.dt_txt.substring(8, 10) == this.props.date.substring(4, 6)){
+        return(
+          <p key={key}><i className={evalIcon(item.weather[0].icon)} /></p>
+        );
+      }
+    }.bind(this));
+
+    var winds = this.props.days.map(function(item, key){
+      if(item.dt_txt.substring(8, 10) == this.props.date.substring(4, 6)){
+        return(
+          <p key={key}>{Math.round(item.wind.speed) + "m/s"}</p>
+        );
+      }
+    }.bind(this));
+
+    var temps = this.props.days.map(function(item, key){
+      if(item.dt_txt.substring(8, 10) == this.props.date.substring(4, 6)){
+        return(
+          <p key={key}>{Math.round(item.main.temp) + "°C"}</p>
+        );
+      }
+    }.bind(this));
+
+    return (
+      <div className="col-sm-4" id="popupDay" style={popupDay}>
+        <div className="row">
+          <div className="col-sm-6">
+            <i className="fa fa-times" style={timesIcon} onClick={this.props.closeDay} />
+          </div>
+          <div className="col-sm-6 text-right">
+            <i className="fa fa-info" style={infoIcon} onClick={this.props.openInfo} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <h1 style={dayTitle} id="dayTitle">{this.props.date}</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-3 text-center">
+            <i className="fa fa-clock-o" style={descIcon} />
+            {times}
+          </div>
+          <div className="col-sm-3 text-center">
+            <i className="wi wi-day-cloudy" style={descIcon} />
+            {weatherIcons}
+          </div>
+          <div className="col-sm-3 text-center">
+            <i className="wi wi-strong-wind" style={descIcon} />
+            {winds}
+          </div>
+          <div className="col-sm-3 text-center">
+            <i className="wi wi-thermometer" style={descIcon} />
+            {temps}
+          </div>
         </div>
       </div>
     );
